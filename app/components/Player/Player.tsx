b602809a6dbc90aa4b00acf836fd8e183edf6fb2
ViacheslavDV <viacheslavdev279@gmail.com>
2023-05-30 13:11:24 +0300

    player fn update

--- app/components/Player/Player.tsx
+++ app/components/Player/Player.tsx
@@ -1,26 +1,33 @@
-import { useMusicQuery } from "@/app/hooks/query/useMusicQuery";
 import styles from "./Player.module.scss";
 import { Howl } from "howler";
 import { TbPlayerPlay, TbPlayerPause } from "react-icons/tb";
+import { MdVolumeUp } from "react-icons/md";
 import { useEffect, useState, useRef } from "react";
 import { useTypedSelector } from "@/app/hooks/useTypedSelector";
 
 const Player: React.FC = () => {
-  const { data } = useMusicQuery();
-  const [currentDuration, setCurrentDuration] = useState(0);
-  const [duration, setDuration] = useState(0);
   const { music } = useTypedSelector((state) => state);
-
-  let sound: Howl | undefined;
+  const [currentDuration, setCurrentDuration] = useState<number>(0);
+  const [songDuration, setSongDuration] = useState<number>(0);
+  const [progressBarWidth, setProgressBarWidth] = useState<number>(0);
+  const [volume, setVolume] = useState(1);
   const soundRef = useRef<Howl>();
+  const intervalRef = useRef<NodeJS.Timeout>();
 
   useEffect(() => {
     if (music.song) {
       soundRef.current = new Howl({
         src: music.song.file,
         html5: true,
+        volume: volume,
         onload: () => {
-          setDuration(soundRef.current?.duration() || 0);
+          setSongDuration(soundRef.current?.duration() || 0);
+        },
+        onplay: () => {
+          intervalRef.current = setInterval(updateSongProgress, 1000);
+        },
+        onpause: () => {
+          clearInterval(intervalRef.current);
         },
       });
     }
@@ -29,28 +36,31 @@ const Player: React.FC = () => {
   const play = () => soundRef.current?.play();
   const pause = () => soundRef.current?.pause();
 
-  useEffect(() => {
-    return () => {
-      if (soundRef.current) {
-        soundRef.current.pause();
-        soundRef.current.unload();
-      }
-    };
-  }, []);
+  // get current song progress
+  const updateSongProgress = () => {
+    setCurrentDuration(soundRef.current?.seek() || 0);
+  };
 
+  // handle song progress
   const handleProgressBar = (event: React.MouseEvent<HTMLElement>) => {
     const progressBarWidth = event.currentTarget.offsetWidth;
     const clickPositionX = event.nativeEvent.offsetX;
-    const newPosition = (clickPositionX / progressBarWidth) * duration;
-    console.log("progressBarWidth:", progressBarWidth);
-    console.log("clickPositionX:", clickPositionX);
-    console.log("newPosition:", newPosition);
+    const newPosition = (clickPositionX / progressBarWidth) * songDuration;
     soundRef.current?.seek(newPosition);
     setCurrentDuration(newPosition);
   };
 
-  console.log("currentDuration:", currentDuration);
-  console.log("duration:", duration);
+  // update progress bar
+  useEffect(() => {
+    setProgressBarWidth((currentDuration * 100) / songDuration);
+  }, [currentDuration]);
+
+  // handle volume
+  useEffect(() => {
+    if (soundRef.current) {
+      soundRef.current.volume(volume);
+    }
+  }, [volume]);
 
   return (
     <section className={styles.wrapper}>
@@ -63,17 +73,30 @@ const Player: React.FC = () => {
             <TbPlayerPause className={styles.icon} />
           </button>
         </div>
-        <div className={styles.progress}>
-          <span>{music.song.title}</span>
-          <span
-            className={styles.progressBar}
-            style={{ width: `${(currentDuration / duration) * 100}%` }}
-            onClick={handleProgressBar}
-          ></span>
-        </div>
-        <div>
-          <span>{currentDuration}</span>
-          <span>{duration}</span>
+        <section className={styles.center}>
+          <span className={styles.duration}>{currentDuration.toFixed(0)}</span>
+          <div className={styles.progress}>
+            <span className={styles.title}>{music.song.title}</span>
+            <div className={styles.progressBar} onClick={handleProgressBar}>
+              <span
+                style={{ width: `${progressBarWidth}%` }}
+                className={styles.songProgress}
+              ></span>
+            </div>
+          </div>
+          <span className={styles.duration}>{songDuration.toFixed(0)}</span>
+        </section>
+        <div className={styles.volume}>
+          <MdVolumeUp className={styles.volumeIcon} />
+          <input
+            className={styles.volumeSlider}
+            type="range"
+            min="0"
+            max="1"
+            step="0.01"
+            value={volume}
+            onChange={(e) => setVolume(Number(e.target.value))}
+          />
         </div>
       </main>
     </section>
